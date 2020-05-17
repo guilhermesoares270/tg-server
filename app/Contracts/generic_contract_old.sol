8e0f6747915d1c6b32715ff2ebd5e519844f92d4
Guilherme Leite <guilherme.soares270@gmail.com>
2020-05-17 12:33:34 -0300

    Remote contract instance

--- app/Contracts/generic_contract_old.sol
+++ app/Contracts/generic_contract_old.sol
@@ -5,7 +5,7 @@ contract Docs {
     struct Doc {
         uint256 id;
         bytes32 signature;
-        bytes32 identity;
+        bytes32 cpf;
     }
 
     string private razao_social;
@@ -32,18 +32,8 @@ contract Docs {
         return DocsList.length;
     }
 
-    function getDoc(bytes32 _signature) public view returns (bytes32) {
-        bytes32 res;
-        for (uint256 i = 0; i < DocsList.length; i++) {
-            if (DocsList[i].signature == _signature) {
-                res = DocsList[i].identity;
-            }
-        }
-        return res;
-    }
-
-    function addDocument(bytes32 _signature, bytes32 _identity) public {
-        Doc memory newDoc = Doc(DocsList.length, _signature, _identity);
+    function addDocument(bytes32 _signature, bytes32 _cpf) public {
+        Doc memory newDoc = Doc(DocsList.length, _signature, _cpf);
         DocsList.push(newDoc);
     }
 
@@ -53,31 +43,33 @@ contract Docs {
         returns (bytes32[] memory, bytes32[] memory)
     {
         bytes32[] memory sigArr = new bytes32[](DocsList.length);
-        bytes32[] memory ideArr = new bytes32[](DocsList.length);
+        bytes32[] memory cpfArr = new bytes32[](DocsList.length);
 
         uint256 arrayLength = DocsList.length;
         for (uint256 i = 0; i < arrayLength; i++) {
             sigArr[i] = DocsList[i].signature;
-            ideArr[i] = DocsList[i].identity;
+            cpfArr[i] = DocsList[i].cpf;
         }
-        return (sigArr, ideArr);
+        return (sigArr, cpfArr);
     }
 
-    function findDocument(bytes32 _signature)
-        public
-        view
-        returns (uint256 id, bytes32 signature)
-    {
-        uint256 resId = 0;
-        bytes32 resSignature = "";
-
+    function validateDocument(bytes32 _signature) public view returns (bool) {
         uint256 arrayLength = DocsList.length;
         for (uint256 i = 0; i < arrayLength; i++) {
             if (_signature == DocsList[i].signature) {
-                resId = DocsList[i].id;
-                resSignature = DocsList[i].signature;
+                return true;
             }
         }
-        return (resId, resSignature);
+        return false;
+    }
+
+    function getDocument(bytes32 _signature) public view returns (bytes32) {
+        uint256 arrayLength = DocsList.length;
+        for (uint256 i = 0; i < arrayLength; i++) {
+            if (_signature == DocsList[i].signature) {
+                return DocsList[i].cpf;
+            }
+        }
+        return "";
     }
 }
